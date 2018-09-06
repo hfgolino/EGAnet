2edcf7d83ea6bf8e1325cd344b316e9e5d9677f0
hfgolino <hfgolino@gmail.com>
2018-09-06 15:18:16 -0400

    New plot function for assessing the replicability of a dimension item-wise.

---
+++ R/itemConfirm.R
@@ -0,0 +1,40 @@
+#'  Plot the replicability of EGA item-wise.
+#'
+#' \code{itemConfirm} Based on the bootEGA results, this function plots the number of times an item (variable) is estimated in the same
+#' factor/dimension as originaly estimated by EGA.
+#'
+#' @param bootega.obj A bootEGA object
+#' @author Hudson F. Golino <hfg9s at virginia.edu>
+#' @examples
+#' \dontrun{
+#' ega.wmt <- EGA(data = wmt2[,7:24], model = "glasso")
+#' boot.wmt <- bootEGA(data = wmt2[,7:24], n = 100, typicalStructure = TRUE,
+#' plot.typicalStructure = TRUE, model = "GGM",
+#' type = "parametric", ncores = 4, confirm = ega.wmt$wc)
+#' itemConfirm(boot.wmt)
+#' }
+#'
+#' \dontrun{
+#' itemConfirm(bootEGA)
+#' }
+#' @seealso \code{\link{EGA}} to estimate the number of dimensions of an instrument using EGA and \code{\link{CFA}} to
+#' verify the fit of the structure suggested by EGA using confirmatory factor analysis.
+#' @export
+
+itemConfirm <- function(bootega.obj){
+  require(ggpubr)
+
+  item.rep <- data.frame(Item = names(bootega.obj$item.confirm),
+                         Rep = bootega.obj$item.confirm)
+
+  ggdotchart(item.rep, x = "Item", y = "Rep",
+             sorting = "descending",
+             add = "segments",
+             rotate = TRUE,
+             dot.size = 6,
+             label = round(item.rep$Rep, 2),
+             font.label = list(color = "white", size = 8,
+                               vjust = 0.5),
+             ggtheme = theme_pubr()
+             )
+}
