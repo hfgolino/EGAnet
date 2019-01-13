818ccf7426adf9e49a71d0b27907fe93eb700896
Alexander Christensen <32655669+AlexChristensen@users.noreply.github.com>
2019-01-13 16:34:17 +0000

    documentation updated

--- R/summary.CFA.R
+++ R/summary.CFA.R
@@ -1,23 +1,30 @@
-#'  Summary method for CFA objects.
+#' Summary for CFA objects
 #'
-#' \code{summary} Returns a summary of the CFA results.
+#' Returns a summary of the CFA results
 #'
-#' @param object An CFA object
-#' @param ... potentially further arguments; unused currently.
+#' @param object An \code{\link[EGA]{CFA}} object
+#'
+#' @param ... potentially further arguments (\strong{unused currently})
+#'
 #' @author Hudson F. Golino <hfg9s at virginia.edu>
+#'
 #' @examples
+#' #estimate EGA
 #' ega.wmt <- EGA(data = wmt2[,7:24], plot.EGA = TRUE)
+#'
+#' #esimtate CFA
 #' cfa.wmt <- CFA(ega.obj = ega.wmt, estimator = 'WLSMV', plot.CFA = TRUE, data = wmt2)
+#'
+#' #summary of CFA results
 #' summary(cfa.wmt)
 #'
-#' \dontrun{
-#' summary(CFA)
-#' }
-#' @seealso \code{\link{EGA}} to estimate the number of dimensions of an instrument using EGA and \code{\link{bootEGA}} to investigate the stability of EGA's estimation via bootstrap.
+#' @seealso \code{\link{EGA}} to estimate the number of dimensions of an instrument using EGA
+#' and \code{\link{bootEGA}} to investigate the stability of EGA's estimation via bootstrap.
 #' @export
-
+#'
 ## S3 method for class 'CFA'
-
+#'
+#Summary function for CFA
 summary.CFA <- function(object, ...) {
   cat("Summary: Confirmatory Factor Analysis:\n")
   print(object$summary)
