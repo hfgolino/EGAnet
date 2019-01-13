502e9de944dcfaa67e915bbfd6978249ac136e42
Alexander Christensen <32655669+AlexChristensen@users.noreply.github.com>
2019-01-13 16:27:53 +0000

    documentation updated

--- R/plot.EGA.R
+++ R/plot.EGA.R
@@ -1,26 +1,36 @@
-#'  Plot method for EGA objects.
+#' Plot method for EGA objects.
 #'
-#' \code{plot} Plots the EGA result using \code{\link{qgraph}}
+#' Plots the EGA result using \code{\link{qgraph}}
 #'
-#' @param x An EGA object
+#' @param x An \code{\link[EGA]{EGA}} object
+#' 
 #' @param title Character. Title of the plot
-#' @param vsize An integer indicating the size of the nodes. Default vsize = 6.
-#' @param ... Arguments passed to 'qgraph'.
+#' 
+#' @param vsize An integer indicating the size of the nodes.
+#' Default vsize = 6
+#' 
+#' @param ... Arguments passed to \code{\link[qgraph]{qgraph}}
+#' 
 #' @author Hudson F. Golino <hfg9s at virginia.edu>
+#' 
 #' @examples
+#' #estimate EGA
 #' ega.wmt <- EGA(data = wmt2[,7:24], plot.EGA = TRUE)
+#' 
+#' #summary of EGA results
 #' summary(ega.wmt)
+#'
+#' #plot EGA network
 #' plot(ega.wmt, vsize = 6)
 #'
-#' \dontrun{
-#' plot(EGA)
-#' }
-#' @seealso \code{\link{EGA}} to estimate the number of dimensions of an instrument using EGA and \code{\link{CFA}} to
-#' verify the fit of the structure suggested by EGA using confirmatory factor analysis.
+#' @seealso \code{\link{EGA}} to estimate the number of dimensions of an instrument using EGA
+#' and \code{\link{CFA}} to verify the fit of the structure suggested by EGA using confirmatory factor analysis.
+#'
 #' @export
-
+#'
 ## S3 method for class 'EGA'
-
+#'
+#Plot EGA function
 plot.EGA <- function(x, title = "", vsize = 6,  ...) {
     plot.ega <- qgraph::qgraph(x$network, layout = "spring", vsize = vsize, groups = as.factor(x$wc), ...)
 
