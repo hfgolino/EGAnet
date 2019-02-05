e22def58e3b177be0880131838287de335693461
Alexander Christensen <32655669+AlexChristensen@users.noreply.github.com>
2019-02-05 13:25:11 +0000

    naming and ordering bug fix

--- R/itemIdent.R
+++ R/itemIdent.R
@@ -45,92 +45,100 @@ itemIdent <- function (bootega.obj, confirm, rep.val = .80, item.rep = .10)
     
     #Strength of within- and between-communities
     net <- bootega.obj$EGA$network
+    
+    ident.item <- function (net, comm)
+    {
+        comc <- NetworkToolbox::comcat(net,comm,metric="each")
+        stab <- NetworkToolbox::stable(net,comm)
         
-        ident.item <- function (net, comm)
+        for(q in 1:nrow(comc))
+        {comc[q,which(is.na(comc[q,]))] <- stab[q]}
+        
+        comm.str <- comc[,order(colnames(comc))]
+        comm.str <- round(comm.str,3)
+        
+        return(comm.str)
+    }
+    
+    item.con <- itemConfirm(bootega.obj,confirm,plot.ic=FALSE)
+    
+    item.lik <- item.con$item.likelihood
+    
+    col <- ncol(item.lik)
+    
+    item.id.samps <- list()
+    
+    max.wc <- max(item.con$wc)
+    
+    for(m in 1:n)
+    {
+        item.id.samps[[m]] <- ident.item(bootega.obj$bootGraphs[[m]], item.con$wc[,m])
+        
+        if(max(item.con$wc[,m])!=max.wc)
         {
-            comc <- NetworkToolbox::comcat(net,comm,metric="each")
-            stab <- NetworkToolbox::stable(net,comm)
+            diff <- max.wc - ncol(item.id.samps[[m]])
             
-            for(q in 1:nrow(comc))
-            {comc[q,which(is.na(comc[q,]))] <- stab[q]}
+            diff.wc <- setdiff(seq(1,max.wc,1),unique(item.con$wc[,m]))
             
-            comm.str <- comc[,order(colnames(comc))]
-            comm.str <- round(comm.str,3)
+            col.names <- c(colnames(item.id.samps[[m]]),paste(diff.wc))
             
-            return(comm.str)
+            for(i in 1:diff)
+            {item.id.samps[[m]] <- cbind(item.id.samps[[m]],rep(NA,nrow(item.id.samps[[m]])))}
+            
+            colnames(item.id.samps[[m]]) <- col.names
+            
+            item.id.samps[[m]] <- item.id.samps[[m]][,order(colnames(item.id.samps[[m]]))]
         }
+    }
+    
+    #Unstandardized
+    unstd.item.id <- round(apply(simplify2array(item.id.samps),1:2, mean, na.rm=TRUE),3)
+    colnames(unstd.item.id) <- paste(seq(1,max(item.con$wc),1))
+    
+    if(ncol(unstd.item.id)!=col)
+    {
+        rm.col <- setdiff(colnames(unstd.item.id),names(item.lik))
         
-        item.con <- itemConfirm(bootega.obj,confirm,plot.ic=FALSE)
+        target.col <- match(rm.col,colnames(unstd.item.id))
         
-        item.lik <- item.con$item.likelihood
-        
-        col <- ncol(item.lik)
-        
-        item.id.samps <- list()
-        
-        max.wc <- max(item.con$wc)
-        
-        for(m in 1:n)
-        {
-            item.id.samps[[m]] <- ident.item(bootega.obj$bootGraphs[[m]], item.con$wc[,m])
-            
-            if(max(item.con$wc[,m])!=max.wc)
-            {
-                diff <- max.wc - max(item.con$wc[,m])
-                
-                for(i in 1:diff)
-                {item.id.samps[[m]] <- cbind(item.id.samps[[m]],rep(NA,nrow(item.id.samps[[m]])))}
-            }
-        }
-        
-        #Unstandardized
-        unstd.item.id <- round(apply(simplify2array(item.id.samps),1:2, mean, na.rm=TRUE),3)
-        colnames(unstd.item.id) <- paste(seq(1,max(item.con$wc),1))
-        
-        if(ncol(unstd.item.id)!=col)
-        {
-            rm.col <- setdiff(colnames(unstd.item.id),names(item.lik))
-            
-            target.col <- match(rm.col,colnames(unstd.item.id))
-            
-            unstd.item.id <- unstd.item.id[,-target.col]
-        }
-        
-        item.lik.ord <- item.lik[match(row.names(unstd.item.id),row.names(item.lik)),]
-        
-        unstd.item.id[which(item.lik.ord=="")] <- ""
-        
-        unstd.item.id <- as.data.frame(unstd.item.id)
-        
-        #Standardized (proportion)
-        #std.item.id <- round(unstd.item.id/rowSums(unstd.item.id),3)
-        
-        
-        #less reliable items
-        con.item <- item.con$item.confirm[row.names(unstd.item.id)]
-        prob.item <- which(con.item<=rep.val)
-        
-        prob.vec <- con.item
-        
-        prob.vec[prob.item] <- "X"
-        prob.vec[-prob.item] <- ""
-        
-        #Unstandardize
-        unstd.item.ident <- as.data.frame(cbind(confirm,unstd.item.id,prob.vec))
-        colnames(unstd.item.ident) <- c("Dimension",colnames(unstd.item.id),paste("Rep<=",rep.val,sep=""))
-        unstd <- unstd.item.ident[match(names(item.con$item.confirm),row.names(unstd.item.ident)),]
-        
-        #Standaridize
-        #std.item.ident <- as.data.frame(cbind(confirm,std.item.id,prob.vec))
-        #colnames(std.item.ident) <- c("Dimension",colnames(std.item.id),paste("Rep<=",rep.val,sep=""))
-        #std <- std.item.ident[order(std.item.ident$Dimension,decreasing=FALSE),]
-        
-        #result <- list()
-        
-        #result$unstd <- unstd
-        #result$std <- std
-        
-        #return(result)
-        
-        return(unstd)
+        unstd.item.id <- unstd.item.id[,-target.col]
+    }
+    
+    item.lik.ord <- item.lik[match(row.names(unstd.item.id),row.names(item.lik)),]
+    
+    unstd.item.id[which(item.lik.ord=="")] <- ""
+    
+    unstd.item.id <- as.data.frame(unstd.item.id)
+    
+    #Standardized (proportion)
+    #std.item.id <- round(unstd.item.id/rowSums(unstd.item.id),3)
+    
+    
+    #less reliable items
+    con.item <- item.con$item.confirm[row.names(unstd.item.id)]
+    prob.item <- which(con.item<=rep.val)
+    
+    prob.vec <- con.item
+    
+    prob.vec[prob.item] <- "X"
+    prob.vec[-prob.item] <- ""
+    
+    #Unstandardize
+    unstd.item.ident <- as.data.frame(cbind(confirm,unstd.item.id,prob.vec))
+    colnames(unstd.item.ident) <- c("Dimension",colnames(unstd.item.id),paste("Rep<=",rep.val,sep=""))
+    unstd <- unstd.item.ident[match(names(item.con$item.confirm),row.names(unstd.item.ident)),]
+    
+    #Standaridize
+    #std.item.ident <- as.data.frame(cbind(confirm,std.item.id,prob.vec))
+    #colnames(std.item.ident) <- c("Dimension",colnames(std.item.id),paste("Rep<=",rep.val,sep=""))
+    #std <- std.item.ident[order(std.item.ident$Dimension,decreasing=FALSE),]
+    
+    #result <- list()
+    
+    #result$unstd <- unstd
+    #result$std <- std
+    
+    #return(result)
+    
+    return(unstd)
 }
