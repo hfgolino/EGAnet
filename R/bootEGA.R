b25569df984db8118eb4bdea32e4dcbd26c42d8f
Hudson Golino <hfgolino@gmail.com>
2019-02-11 10:55:08 -0500

    Corrected the output for typicalStructure. Now, if if (typicalStructure == FALSE), the typical.Structure object won't be part of the result. This solves a problem we faced with simulated data.

--- R/bootEGA.R
+++ R/bootEGA.R
@@ -6,102 +6,102 @@
 #' over the \emph{n} bootstraps) and its dimensionality.
 #'
 #' @param data A dataframe with the variables to be used in the analysis
-#' 
+#'
 #' @param n An integer value representing the number of bootstraps
-#' 
+#'
 #' @param model A string indicating the method to use.
 #' Current options are:
-#' 
+#'
 #' \itemize{
-#' 
+#'
 #' \item{\strong{\code{glasso}}}
 #' {Estimates the Gaussian graphical model using graphical LASSO with
 #' extended Bayesian information criterion to select optimal regularization parameter.
 #' This is the default method}
-#' 
+#'
 #' \item{\strong{\code{TMFG}}}
 #' {Estimates a Triangulated Maximally Filtered Graph}
-#' 
+#'
 #' }
-#' 
+#'
 #' @param type A string indicating the type of bootstrap to use.
 #' Current options are:
-#' 
+#'
 #' \itemize{
-#' 
+#'
 #' \item{\strong{\code{parametric}}}
 #' {Generates n new datasets (multivariate normal random distributions) based on the
 #' original dataset, via the \code{\link[mvtnorm]{Mvnorm}} function of the mvtnorm package}
-#' 
+#'
 #' \item{\strong{\code{resampling}}}
 #' {Generates n random subsamples of the original data}
-#' 
+#'
 #' }
-#' 
+#'
 #' @param typicalStructure Logical.
 #' If true, returns the typical network of partial correlations
 #' (estimated via graphical lasso or via TMFG) and estimates its dimensions.
 #' The "typical network" is the median of all pairwise correlations over the \emph{n} bootstraps.
 #' Defaults to TRUE
-#' 
+#'
 #' @param plot.typicalStructure Logical.
 #' If true, returns a plot of the typical network (partial correlations),
 #' which is the median of all pairwise correlations over the \emph{n} bootstraps,
 #' and its estimated dimensions.
 #' Defaults to TRUE
-#' 
+#'
 #' @param ncores Number of cores to use in computing results.
 #' Set to 1 to not use parallel computing
-#' 
+#'
 #' @param ... Additional arguments to be passed to \code{\link[EGA]{EBICglasso.qgraph}}
 #' or \code{\link[NetworkToolbox]{TMFG}}
-#' 
+#'
 #' @return Returns a list containing:
-#' 
+#'
 #' \item{n}{Number of replica samples in bootstrap}
-#' 
+#'
 #' \item{boot.ndim}{Number of dimensions identified in each replica sample}
-#' 
+#'
 #' \item{boot.wc}{Item allocation for each replica sample}
-#' 
+#'
 #' \item{bootGraphs}{Networks of each replica sample}
-#' 
+#'
 #' \item{summary.table}{Summary table containing number of replica samples, median,
 #' standard deviation, standard error, and 95\% confidence intervals}
-#' 
+#'
 #' \item{likelihood}{Proportion of times the number of dimensions was identified
 #' (e.g., .85 of 1,000 = 850 times that specific number of dimensions was found)}
-#' 
+#'
 #' \item{EGA}{Output of the original \code{\link[EGA]{EGA}} reults}
-#' 
+#'
 #' \item{typicalGraph}{A list containing:
-#' 
+#'
 #' \itemize{
-#' 
+#'
 #' \item{\strong{\code{graph}}}
 #' {Network matrix of the median network structure}
-#' 
+#'
 #' \item{\strong{\code{typical.dim.variables}}}
 #' {An ordered matrix of item allocation}
-#' 
+#'
 #' \item{\strong{\code{wc}}}
 #' {Item allocation of the median network}
-#' 
+#'
 #' }}
-#' 
+#'
 #' @author Hudson F. Golino <hfg9s at virginia.edu> and Alexander P. Christensen <alexpaulchristensen@gmail.com>
-#' 
+#'
 #' @examples
 #' \dontrun{
 #' #bootEGA glasso example
 #' boot.wmt <- bootEGA(data = wmt2[,7:24], n = 500, typicalStructure = TRUE,
 #' plot.typicalStructure = TRUE, model = "glasso", type = "parametric", ncores = 4)
-#' 
+#'
 #' #bootEGA TMFG example
 #' boot.intwl <- bootEGA(data = intelligenceBattery[,8:66], n = 500, typicalStructure = TRUE,
 #' plot.typicalStructure = TRUE, model = "TMFG", type = "parametric", ncores = 4)
 #' }
-#' 
+#'
 #' @seealso \code{\link{EGA}} to estimate the number of dimensions of an instrument using EGA
 #' and \code{\link{CFA}} to verify the fit of the structure suggested by EGA using confirmatory factor analysis.
 #'
@@ -109,12 +109,12 @@
 #' @importFrom stats cov median sd qt
 #'
 #' @export
-#' 
+#'
 # Bootstrap EGA
 bootEGA <- function(data, n,
                     model = c("glasso", "TMFG"), type = c("parametric", "resampling"),
                     typicalStructure = TRUE, plot.typicalStructure = TRUE, ncores = 4, ...) {
-    
+
     #set inverse covariance matrix for parametric approach
     if(type=="parametric")  # Use a parametric approach:
     {
@@ -128,18 +128,18 @@ bootEGA <- function(data, n,
             diag(g) <- 1
         }
     }
-    
+
     #Parallel processing
     cl <- parallel::makeCluster(ncores)
     doParallel::registerDoParallel(cl)
-    
+
     #progress bar
     #pb <- txtProgressBar(max=n, style = 3)
     #progress <- function(num) setTxtProgressBar(pb, num)
     #opts <- list(progress = progress)
-    
+
     boots <- list()
-    
+
     #nets
     boots <-foreach::foreach(i=1:n,
                              .packages = c("NetworkToolbox","psych","qgraph","EGA")
@@ -149,25 +149,25 @@ bootEGA <- function(data, n,
             if(type=="parametric")  # Use a parametric approach:
             {
                 bootData <- mvtnorm::rmvnorm(nrow(data), sigma = corpcor::pseudoinverse(g))
-                
+
                 if(model=="glasso")
                 {net <- EBICglasso.qgraph(cov(bootData), n = nrow(data), lambda.min.ratio = 0.1, returnAllResults = FALSE, ...)
                 }else if(model=="TMFG")
                 {net <- NetworkToolbox::TMFG(bootData, normal = TRUE, na.data = "pairwise", ...)$A}
-                
+
             }else if(type=="resampling") # Random subsample with replace
             {
                 mat <- data[sample(1:nrow(data), replace=TRUE),]
-                
+
                 if(model=="glasso")
                 {net <- EBICglasso.qgraph(qgraph::cor_auto(mat), n=nrow(data), lambda.min.ratio = 0.1, returnAllResults = FALSE, ...)
                 }else if(model=="TMFG")
                 {net <- NetworkToolbox::TMFG(mat, normal = TRUE, na.data = "pairwise", ...)$A}
             }
     }
-    
+
     parallel::stopCluster(cl)
-    
+
     bootGraphs <- vector("list", n)
     for (i in 1:n) {
         bootGraphs[[i]] <- boots[[i]]
@@ -186,9 +186,9 @@ bootEGA <- function(data, n,
     for (m in 1:n) {
         boot.ndim[m, 2] <- max(boot.wc[[m]]$membership)
     }
-    
+
     colnames(boot.ndim) <- c("Boot.Number", "N.Dim")
-    
+
     boot.ndim[, 1] <- seq_len(n)
     if (typicalStructure == TRUE) {
         if(model=="glasso")
@@ -212,20 +212,20 @@ bootEGA <- function(data, n,
     summary.table <- data.frame(n.Boots = n, median.dim = Median,
                                 SD.dim = sd.boot, SE.dim = se.boot, CI.dim = ci, Lower = Median -
                                     ci, Upper = Median + ci)
-    
+
     #compute likelihood
     dim.range <- range(boot.ndim[,2])
     lik <- matrix(0, nrow = diff(dim.range)+1, ncol = 2)
     colnames(lik) <- c("# of Factors", "Likelihood")
     count <- 0
-    
+
     for(i in seq(from=min(dim.range),to=max(dim.range),by=1))
     {
         count <- count + 1
         lik[count,1] <- i
         lik[count,2] <- length(which(boot.ndim[,2]==i))/n
     }
-    
+
     result <- list()
     result$n <- n
     result$boot.ndim <- boot.ndim
@@ -234,11 +234,13 @@ bootEGA <- function(data, n,
     result$summary.table <- summary.table
     result$likelihood <- lik
     result$EGA <- suppressMessages(suppressWarnings(EGA(data = data, model = model, plot.EGA = FALSE)))
+    if (typicalStructure == TRUE) {
     typicalGraph <- list()
     typicalGraph$graph <- typical.Structure
     typicalGraph$typical.dim.variables <- dim.variables[order(dim.variables[,2]), ]
     typicalGraph$wc <- typical.wc$membership
     result$typicalGraph <- typicalGraph
+    }
     class(result) <- "bootEGA"
     return(result)
 }
