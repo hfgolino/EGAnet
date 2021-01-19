d00bfe65af29c114b5b086e53dc16afc34f1b129
Hudson Golino <hfgolino@gmail.com>
2021-01-19 10:33:04 -0500

    Updated zzz object.

--- R/zzz.R
+++ R/zzz.R
@@ -10,7 +10,8 @@
                temp$Date,"\n", sep="")
     msg <- paste(msg,"Maintainer: Hudson Golino, University of Virginia\n",sep="")
     msg <- paste(msg, "Authors: Hudson Golino, University of Virginia & \n Alexander Christensen, University of Pennsylvania\n",sep="")
-    msg <- paste(msg,"Contributors: Robert Moulder, University of Virginia\n",sep="")
+    msg <- paste(msg,"Contributors: Robert Moulder, University of Virginia,\n",
+                 "Luis Eduardo Garrido, Pontificia Universidad Catolica Madre y Maestra\n",sep="")
     msg <- paste(msg,'For citation information, type citation("EGAnet")\n')
     msg <- paste(msg,'For vignettes, type browseVignettes("EGAnet")\n')
     msg <- paste(msg,"For bugs and errors, submit an issue to <https://github.com/hfgolino/EGAnet/issues>")
