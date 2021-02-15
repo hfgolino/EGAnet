b7b86923fa73dab0dc0bd8bf781547d4bf446efa
Hudson Golino <hfgolino@gmail.com>
2021-02-15 14:44:15 -0500

    Bug fix totalCorMat

--- R/totalCorMat.R
+++ R/totalCorMat.R
@@ -4,7 +4,7 @@
 #'
 #' @param data Matrix or data frame.
 #' Variables to be used in the analysis
-#' 
+#'
 #' @return Returns a square matrix with pairwise total correlations
 #'
 #' @author Hudson F. Golino <hfg9s at virginia.edu>
@@ -12,13 +12,13 @@
 #' @examples
 #' # Compute total correlation
 #' totalCorMat(wmt2[,7:24])
-#' 
-#' @references 
+#'
+#' @references
 #' Watanabe, S. (1960).
 #' Information theoretical analysis of multivariate correlation.
 #' \emph{IBM Journal of Research and Development} \emph{4}, 66-82.
-#' 
-#' 
+#'
+#'
 #' @export
 #'
 # Total COrrelation
@@ -27,7 +27,7 @@ totalCorMat <- function(data){
   TotCorMat <- matrix(NA,nrow=ncol(data),ncol=ncol(data))
   TotCorMat <- matrix(apply(expand.grid( x = 1:ncol(data), y = 1:ncol(data)), 1,
                               function(x){ return(totalCor( data[ , c(x[1], x[2])])$Total.Cor)}),
-                       nc = ncol(data))
+                      ncol = ncol(data))
   colnames(TotCorMat) <- colnames(data)
   rownames(TotCorMat) <- colnames(data)
   return(TotCorMat)
