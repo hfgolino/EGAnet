764e31ff0c8616e406e00ad5729fd53eb0b8f50e
Alexander Christensen <alexpaulchristensen@gmail.com>
2021-03-12 07:26:19 -0500

    Update bootEGA.R

--- R/bootEGA.R
+++ R/bootEGA.R
@@ -318,9 +318,6 @@ bootEGA <- function(data, uni.method = c("expand", "LE"), iter,
   }
 
   #### DEPRECATED ARGUMENTS ####
-  
-  # Message function
-  message(styletext(styletext("\nBootstrap Exploratory Graph Analysis", defaults = "underline"), defaults = "bold"))
 
   #### MISSING ARGUMENTS HANDLING ####
   
@@ -405,9 +402,12 @@ bootEGA <- function(data, uni.method = c("expand", "LE"), iter,
 
   #### MISSING ARGUMENTS HANDLING ####
   
+  # Message function
+  message(styletext(styletext("\nBootstrap Exploratory Graph Analysis", defaults = "underline"), defaults = "bold"))
+  
   # Let user know setting
   message(paste(" \u2022 type = ", type, "\n",
-                " \u2022 iterations = ", iter,
+                " \u2022 iterations = ", iter, "\n",
                 sep=""))
 
   #number of cases
