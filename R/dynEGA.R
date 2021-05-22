9131c68869e88b77c3567520014b5163754fd057
Alexander Christensen <alexpaulchristensen@gmail.com>
2021-05-22 12:20:15 -0400

    Update dynEGA.R

--- R/dynEGA.R
+++ R/dynEGA.R
@@ -266,7 +266,7 @@ dynEGA <- function(data, n.embed, tau = 1, delta = 1,
 
   #Export variables
   parallel::clusterExport(cl = cl,
-                          varlist = c("datalist", "derivlist", "cases", "glla"),
+                          varlist = c("datalist", "derivlist", "cases"),
                           envir=environment())
 
   # GLLA Estimation:
