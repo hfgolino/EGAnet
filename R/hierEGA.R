bf2ade0d6f2ec57503f05c83094b6841c3549d30
Alexander Christensen <alexpaulchristensen@gmail.com>
2022-05-23 18:18:12 -0400

    Update hierEGA.R

--- R/hierEGA.R
+++ R/hierEGA.R
@@ -795,7 +795,7 @@ hierEGA <- function(
     length(hierarchical$higher_order$EGA$wc) ==
     length(na.omit(unique(hierarchical$higher_order$EGA$wc)))
   ){
-    message("No general dimensions were identified.")
+    message("No general dimensions were identified. Lower order solution represents major factors.")
     hierarchical$higher_order$EGA$n.dim <- 0
   }
 
