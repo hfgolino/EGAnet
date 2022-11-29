b030b25f0628bb30e88f19f47dfb43f5549182df
hfgolino <hfgolino@gmail.com>
2022-11-29 12:50:22 -0500

    CFA function updated to handle hierEGA objects

--- R/CFA.R
+++ R/CFA.R
@@ -1,8 +1,8 @@
-#' CFA Fit of \code{\link[EGAnet]{EGA}} Structure
+#' CFA Fit of \code{\link[EGAnet]{EGA}} or \code{\link[EGAnet]{hierEGA}} Structure
 #'
-#' Verifies the fit of the structure suggested by \code{\link[EGAnet]{EGA}} using confirmatory factor analysis
+#' Verifies the fit of the structure suggested by \code{\link[EGAnet]{EGA}} or by \code{\link[EGAnet]{hierEGA}} using confirmatory factor analysis
 #'
-#' @param ega.obj An \code{\link[EGAnet]{EGA}} object
+#' @param ega.obj An \code{\link[EGAnet]{EGA}} object or an \code{\link[EGAnet]{hierEGA}}
 #'
 #' @param data A dataframe with the variables to be used in the analysis
 #'
@@ -112,6 +112,7 @@ CFA<- function(ega.obj, data, estimator, plot.CFA = TRUE, layout = "spring", ...
   }
   
   
+  if(class(ega.obj)=="EGA"){
     strct <- split(ega.obj$dim.variables[, 1], list(ega.obj$dim.variables[, 2]))
     names(strct) <- paste("Fat", labels(strct))
     model.ega <- paste(names(strct), " =~ ", lapply(strct, function(x) paste(print(x), collapse = " + ")), collapse = " \n ")
@@ -122,20 +123,60 @@ CFA<- function(ega.obj, data, estimator, plot.CFA = TRUE, layout = "spring", ...
       what = "FUN",
       args = lavaan.args
     )
-    summary.cfa <- summary(fit.mod.ega, fit.measures = TRUE)
-
-    if (estimator == "WLSMV") {
-        fit.measures.cfa <- lavaan::fitMeasures(fit.mod.ega, fit.measures = c("chisq.scaled", "df", "pvalue", "cfi.scaled", "rmsea.scaled"))
-    } else {
-        fit.measures.cfa <- lavaan::fitMeasures(fit.mod.ega, fit.measures = c("chisq", "df", "pvalue", "cfi", "rmsea", "gfi", "nfi"))
+}
+  if(class(ega.obj)=="hierEGA"){
+     ## Organizing 1st level items and factors:
+    hierEGA.obj <- ega.obj
+    
+    strct <- split(hierEGA.obj$lower_order$dim.variables[, 1], list(hierEGA.obj$lower_order$dim.variables[, 2]))
+    names(strct) <- paste("FirstFact", labels(strct))
+    
+    ## Organizing 2nd level items and factors:
+    
+    dim.variables.second <- hierEGA.obj$lower_order$dim.variables
+    dim.variables.second[,2] <- hierEGA.obj$higher_order$EGA$wc[as.character(hierEGA.obj$lower_order$wc)]
+    
+    strct.2nd <- split(dim.variables.second[, 1], list(paste("GenFact", dim.variables.second[, 2])))
+    
+    if(length(names(strct.2nd))>1){
+      ## Pairing the names of the 2nd level factors:
+      pair.names <- t(combn(names(strct.2nd),2))
+      
+      ## Lavaan Model based on hierEGA results:
+      model.ega <- paste(paste(names(strct), " =~ ", lapply(strct, function(x) paste(print(x), collapse = " + ")), collapse = " \n "), 
+                         paste(names(strct.2nd), " =~ ", lapply(strct.2nd, function(x) paste(print(x), collapse = " + ")), collapse = " \n "), 
+                         paste(apply(pair.names, 1, function(x) paste(x[1],"~~", x[2], sep = " ")), collapse = " \n "), sep = " \n ")  
+    }else{
+      ## Lavaan Model based on hierEGA results:
+      model.ega <- paste(paste(names(strct), " =~ ", lapply(strct, function(x) paste(print(x), collapse = " + ")), collapse = " \n "), 
+                         paste(names(strct.2nd), " =~ ", lapply(strct.2nd, function(x) paste(print(x), collapse = " + ")), collapse = " \n "), sep = " \n ") 
     }
+    
+    lavaan.args$model <- model.ega
+    lavaan.args$data <- data
+    lavaan.args$orthogonal <- TRUE
 
-    if (plot.CFA == TRUE) {
-        plot.cfa <- semPlot::semPaths(fit.mod.ega, title = FALSE, label.cex = 0.8, sizeLat = 8, sizeMan = 5, edge.label.cex = 0.6, minimum = 0.1,
-                                      sizeInt = 0.8, mar = c(1, 1, 1, 1), residuals = FALSE, intercepts = FALSE, thresholds = FALSE, layout = "spring",
-                                      "std", cut = 0.5)
+    FUN <- lavaan::cfa
+    fit.mod.ega <- do.call(
+      what = "FUN",
+      args = lavaan.args
+    )
     }
-
+  
+  summary.cfa <- summary(fit.mod.ega, fit.measures = TRUE)
+  
+  if (estimator == "WLSMV") {
+    fit.measures.cfa <- lavaan::fitMeasures(fit.mod.ega, fit.measures = c("chisq.scaled", "df", "pvalue", "cfi.scaled", "rmsea.scaled"))
+  } else {
+    fit.measures.cfa <- lavaan::fitMeasures(fit.mod.ega, fit.measures = c("chisq", "df", "pvalue", "cfi", "rmsea", "gfi", "nfi"))
+  }
+  
+  if (plot.CFA == TRUE) {
+    plot.cfa <- semPlot::semPaths(fit.mod.ega, title = FALSE, label.cex = 0.8, sizeLat = 8, sizeMan = 5, edge.label.cex = 0.6, minimum = 0.1,
+                                  sizeInt = 0.8, mar = c(1, 1, 1, 1), residuals = FALSE, intercepts = FALSE, thresholds = FALSE, layout = "spring",
+                                  "std", cut = 0.5)
+  }
+  
     cfa <- list()
     cfa$fit <- fit.mod.ega
     cfa$summary <- summary.cfa
