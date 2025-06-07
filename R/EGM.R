2fa81d85c14c34f3c7f6a973488682b2197d9d31
Alexander Christensen <alexpaulchristensen@gmail.com>
2025-06-07 05:43:42 -0500

    Update EGM.R

--- R/EGM.R
+++ R/EGM.R
@@ -477,7 +477,7 @@ EGM.explore <- function(data, communities, search, iter, optimize.network, opt, 
   )
 
   # Obtain partial correlations that are greater than random chance
-  null_P <- wto(empirical_P)
+  null_P <- empirical_P * (abs(empirical_P) >= abs(expected_edges(empirical_P)))
 
   # Collect results
   results <- lapply(
