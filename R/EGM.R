2b4ea08cfccfe9a94a9bc763faa21bed3ed46d61
Alexander Christensen <alexpaulchristensen@gmail.com>
2025-07-20 13:49:37 -0500

    Update EGM.R

--- R/EGM.R
+++ R/EGM.R
@@ -1382,7 +1382,8 @@ EGM.explore.core <- function(
   loadings[] <- initial_loadings$par
 
   # Set lambda sequence
-  lambdas <- seq(0.05, 0.001, 0.001)
+  # (reverse order ensures higher lambda is used when there is a tie)
+  lambdas <- seq(0.05, 0.001, -0.001)
 
   # Optimize SCAD soft threshold
   soft_threshold <- nvapply(
