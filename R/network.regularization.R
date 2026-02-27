8511865b2016600844764c1b9fbc57d056a20ee0
Alexander Christensen <alexpaulchristensen@gmail.com>
2026-02-27 09:11:20 -0600

    Update network.regularization.R

--- R/network.regularization.R
+++ R/network.regularization.R
@@ -444,16 +444,13 @@ network.regularization <- function(
 
       }else if(penalty == "exp"){
 
-        # Obtain median of distribution
-        # gamma <- log(2) * sum(lower_P) / sum(lower_triangle)
-        # gamma <- sum(lower_P) / sum(lower_triangle)
+        # Obtain MLE of scale
         gamma <- mean(lower_P)
 
       }else if(penalty == "gumbel"){
 
-        # Estimate scale parameter for mean
-        gamma <- gumbel_mle(lower_P) # * -digamma(1) # 0.5772156649
-        # uses Euler-Mascheroni constant
+        # Estimate MLE scale parameter
+        gamma <- gumbel_mle(lower_P)
 
       }else if(penalty == "weibull"){
 
@@ -463,8 +460,6 @@ network.regularization <- function(
         # Set parameters
         shape <- min(estimates[["shape"]], 1) # cap at EXP
         gamma <- estimates[["scale"]]
-        # gamma <- estimates[["scale"]] * log(2)^(1 / shape) # median
-        # gamma <- estimates[["scale"]] * gamma(1 + 1 / shape)
 
       }
 
